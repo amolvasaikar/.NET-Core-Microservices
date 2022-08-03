945ab93a456c06efc22957e8b1ca931f4486f35c
Amol Vasaikar <amol.vasaikar1@gmail.com>
2022-08-03 10:47:22 +0530

    ProductAPI Created
    ProductAPI Created

---
+++ MongoRestaurant/Mongo.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mongo.Services.ProductAPI.Models;
+
+namespace Mongo.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MongoRestaurant/Mongo.Services.ProductAPI/Models/DTO/ResponceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mongo.Services.ProductAPI.Models.DTO
+{
+    public class ResponceDto
+    {
+        public bool IsSucess { get; set; } = true;
+        public object Result  { get; set; }
+        public string DisplayMessage { get; set; }
+        public List<string> ErrorMessages { get; set; }
+
+    }
+}
